3e07a6931d501ae44992df6f9034a5d6bcfea370
whitesoup12 <whitesoup12@gmail.com>
2024-08-26 10:11:24 -0500

    Change client IP to config to easily update

--- src/main/java/com/remotefalcon/viewer/util/ClientUtil.java
+++ src/main/java/com/remotefalcon/viewer/util/ClientUtil.java
@@ -2,15 +2,19 @@ package com.remotefalcon.viewer.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ClientUtil {
+  @Value("${client.header}")
+  String clientHeader;
+
   public String getClientIp(HttpServletRequest request) {
     String remoteAddr = "";
     if (request != null) {
-      remoteAddr = request.getHeader("CF-Connecting-IP");
+      remoteAddr = request.getHeader(clientHeader);
       if (remoteAddr == null || "".equals(remoteAddr)) {
         remoteAddr = request.getRemoteAddr();
       }
