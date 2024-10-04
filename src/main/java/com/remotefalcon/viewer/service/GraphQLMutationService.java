77ee9585d922832fe9c6386066634030edf6ff3a
whitesoup12 <whitesoup12@users.noreply.github.com>
2024-10-04 09:06:07 -0500

    Add some logging for testing.

--- src/main/java/com/remotefalcon/viewer/service/GraphQLMutationService.java
+++ src/main/java/com/remotefalcon/viewer/service/GraphQLMutationService.java
@@ -14,6 +14,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GraphQLMutationService {
     private final AuthUtil authUtil;
     private final ClientUtil clientUtil;
@@ -292,6 +294,7 @@ public class GraphQLMutationService {
                     .position(request.getPosition() + 1)
                     .build()));
         }
+        log.info("Successfully requested {} for {}", requestedSequence.getDisplayName(), show.getShowName());
         this.showRepository.save(show);
     }
 
